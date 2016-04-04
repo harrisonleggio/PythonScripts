da55e0fadca4789d62f79d08c5bbb9d7e310f5ed
Harrison Leggio <harrisonleggio@my.uri.edu>
2016-04-04 18:12:04 -0400

    Gets 20 most recent tweets by a user

---
+++ getUserTweets.py
@@ -0,0 +1,27 @@
+import tweepy
+
+CONSUMER_KEY = 'sFLL9lXR0ZVUU5P7F26g7tds5'
+CONSUMER_SECRET = 'oYNS95rzjWoPDXOfGAW1FzsACSaG2iT6pFBZ4rdVTYN25Rz3nl'
+ACCESS_KEY = '717037162903040000-rbf0HtqbhnP8howTSXdMlkt02Vp7fk9'
+ACCESS_SECRET = 'Sm5EEAuH2pMD3KxxFwfYPAgOs0AhXYloTvUR6aYwfbrmM'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+harrison = api.user_timeline('harrisonleggio')
+i = 0
+for tweet in harrison:
+    print (str(i) + ': ' + tweet.text)
+    i += 1
+
+
+
+
+
+
+
+
+
+
+
