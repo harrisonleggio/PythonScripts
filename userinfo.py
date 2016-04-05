191e30f2da73dc4cf212980bf4ab399e7e6b326b
Harrison Leggio <harrisonleggio@my.uri.edu>
2016-04-05 19:36:18 -0400

    Finds 20 most recent users someone is following

---
+++ userinfo.py
@@ -0,0 +1,35 @@
+import tweepy
+
+CONSUMER_KEY = 'sFLL9lXR0ZVUU5P7F26g7tds5'
+CONSUMER_SECRET = 'oYNS95rzjWoPDXOfGAW1FzsACSaG2iT6pFBZ4rdVTYN25Rz3nl'
+ACCESS_KEY = '717037162903040000-rbf0HtqbhnP8howTSXdMlkt02Vp7fk9'
+ACCESS_SECRET = 'Sm5EEAuH2pMD3KxxFwfYPAgOs0AhXYloTvUR6aYwfbrmM'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+user = api.get_user('harrisonleggio')
+
+print ('Username: ' + user.screen_name)
+print ('Following: ' + str(user.followers_count))
+for friend in user.friends():
+    print (friend.screen_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
