3175cffc0537975162660bb6cea2db0b1f528a8c
Harrison Leggio <harrisonleggio@my.uri.edu>
2016-04-06 21:52:22 -0400

    Changed how tweets are displayed in window.

--- client.py
+++ client.py
@@ -18,15 +18,9 @@ choices = ["Search For Someone","Tweet", "Read Timeline"]
 
 def readTimeline():
     tweets = api.home_timeline()
-    msgbox(
-            tweets[0].text + "\n\n" + tweets[1].text + "\n\n" + tweets[2].text +
-           "\n\n" + tweets[3].text + "\n\n" +tweets[4].text + "\n\n" +tweets[5].text +
-           "\n\n" + tweets[6].text + "\n\n" + tweets[7].text + "\n\n" + tweets[8].text +
-            "\n\n" + tweets[9].text + "\n\n" + tweets[10].text + "\n\n" + tweets[11].text
-            + "\n\n" + tweets[12].text + "\n\n" + tweets[13].text + "\n\n" + tweets[14].text
-            + "\n\n" + tweets[15].text + "\n\n" + tweets[16].text + "\n\n" + tweets[17].text
-           + "\n\n" + tweets[18].text + "\n\n" + tweets[19].text)
-
+    for tweet in tweets:
+        tweets = buttonbox(title="Twitter Feed", msg = tweet.text, choices = ("Next", "Exit"))
+ 
 def tweet():
     new_tweet = enterbox(msg = "What would you like to tweet?", title="Post Tweet")
     if len(new_tweet) > 140:
@@ -38,14 +32,8 @@ def tweet():
 def user():
     twitteruser = enterbox(msg = "Who would you like to search for?", title = "Search")
     tweets = api.user_timeline(twitteruser)
-    msgbox(
-            tweets[0].text + "\n\n" + tweets[1].text + "\n\n" + tweets[2].text +
-           "\n\n" + tweets[3].text + "\n\n" +tweets[4].text + "\n\n" +tweets[5].text +
-           "\n\n" + tweets[6].text + "\n\n" + tweets[7].text + "\n\n" + tweets[8].text +
-            "\n\n" + tweets[9].text + "\n\n" + tweets[10].text + "\n\n" + tweets[11].text
-            + "\n\n" + tweets[12].text + "\n\n" + tweets[13].text + "\n\n" + tweets[14].text
-            + "\n\n" + tweets[15].text + "\n\n" + tweets[16].text + "\n\n" + tweets[17].text
-           + "\n\n" + tweets[18].text + "\n\n" + tweets[19].text)
+    for tweet in tweets:
+        tweets = buttonbox(title=twitteruser + "'s Tweets", msg = tweet.text, choices = ("Next", "Exit"))
 
 while True:
     choice = choicebox(msg, title, choices)
